e7a43f131981e71ae3c66b03117952bf864abd35
Justin Challenger <challenger.justin@gmail.com>
2021-06-30 16:57:46 -0400

    adds common tool installs

---
+++ install-workspace.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# VS Code
+sudo snap install --classic code 
+
+# Install tools and utilities
+sudo apt install ssh git keychain curl 
+
+# Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Oh My Bash installer
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+sed -i 's/OSH_THEME=".*"/OSH_THEME="brainy"/g' ~/.bashrc
